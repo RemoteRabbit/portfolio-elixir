b380945d6a45828882d28a71d8dabb7b12fe958d
Tristan Jahnke <trjahnke@gmail.com>
2023-09-19 21:33:17 -0500

    add certs and what Im good at

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,21 +1,80 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-const fs = require("fs")
-const path = require("path")
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   content: [
     "./js/**/*.js",
     "../lib/*_web.ex",
-    "../lib/*_web/**/*.*ex"
+    "../lib/*_web/**/*.*ex",
   ],
   theme: {
     extend: {
       colors: {
-        brand: "#FD4F00",
-      }
+        "charcoal": {
+          DEFAULT: "#394053",
+          100: "#0b0d10",
+          200: "#161921",
+          300: "#212631",
+          400: "#2d3242",
+          500: "#394053",
+          600: "#56617e",
+          700: "#7a85a5",
+          800: "#a6aec3",
+          900: "#d3d6e1",
+        },
+        "anti-flash_white": {
+          DEFAULT: "#eef0f2",
+          100: "#2a3036",
+          200: "#53606c",
+          300: "#81909e",
+          400: "#b8c0c8",
+          500: "#eef0f2",
+          600: "#f1f3f4",
+          700: "#f5f6f7",
+          800: "#f8f9fa",
+          900: "#fcfcfc",
+        },
+        "silver": {
+          DEFAULT: "#c6c7c4",
+          100: "#282826",
+          200: "#4f514c",
+          300: "#777972",
+          400: "#9ea09a",
+          500: "#c6c7c4",
+          600: "#d1d1cf",
+          700: "#dcdddb",
+          800: "#e8e8e7",
+          900: "#f3f4f3",
+        },
+        "rose_quartz": {
+          DEFAULT: "#a2999e",
+          100: "#211e20",
+          200: "#423c40",
+          300: "#645a5f",
+          400: "#85787f",
+          500: "#a2999e",
+          600: "#b5aeb2",
+          700: "#c8c2c5",
+          800: "#dad6d8",
+          900: "#edebec",
+        },
+        "cornell_red": {
+          DEFAULT: "#a8201a",
+          100: "#220705",
+          200: "#430d0a",
+          300: "#651410",
+          400: "#861b15",
+          500: "#a8201a",
+          600: "#dd2d24",
+          700: "#e5625b",
+          800: "#ee9692",
+          900: "#f6cbc8",
+        },
+      },
     },
   },
   plugins: [
@@ -25,31 +84,51 @@ module.exports = {
     //
     //     <div class="phx-click-loading:animate-ping">
     //
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ])
+    ),
 
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
-    plugin(function({matchComponents, theme}) {
-      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
-      let values = {}
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized");
+      let values = {};
       let icons = [
         ["", "/24/outline"],
         ["-solid", "/24/solid"],
-        ["-mini", "/20/solid"]
-      ]
+        ["-mini", "/20/solid"],
+      ];
       icons.forEach(([suffix, dir]) => {
-        fs.readdirSync(path.join(iconsDir, dir)).map(file => {
-          let name = path.basename(file, ".svg") + suffix
-          values[name] = {name, fullPath: path.join(iconsDir, dir, file)}
-        })
-      })
+        fs.readdirSync(path.join(iconsDir, dir)).map((file) => {
+          let name = path.basename(file, ".svg") + suffix;
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
+        });
+      });
       matchComponents({
-        "hero": ({name, fullPath}) => {
-          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
+        "hero": ({ name, fullPath }) => {
+          let content = fs.readFileSync(fullPath).toString().replace(
+            /\r?\n|\r/g,
+            "",
+          );
           return {
             [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
             "-webkit-mask": `var(--hero-${name})`,
@@ -59,10 +138,10 @@ module.exports = {
             "vertical-align": "middle",
             "display": "inline-block",
             "width": theme("spacing.5"),
-            "height": theme("spacing.5")
-          }
-        }
-      }, {values})
-    })
-  ]
-}
+            "height": theme("spacing.5"),
+          };
+        },
+      }, { values });
+    }),
+  ],
+};
