1de0b9a02b5b36a090d7ad516f6a2e7aeb272e73
Tristan Jahnke <trjahnke@protonmail.com>
2024-11-04 19:48:33 -0600

    feat: list posts
    Added it so when you go to `/blog` it will lists out all published posts

--- lib/portfolio_web/controllers/page_controller.ex
+++ lib/portfolio_web/controllers/page_controller.ex
@@ -1,12 +1,14 @@
 defmodule PortfolioWeb.PageController do
   use PortfolioWeb, :controller
+  alias Portfolio.Blog
 
   def home(conn, _params) do
     render(conn, :home)
   end
 
   def blog(conn, _params) do
-    render(conn, :blog)
+    posts = Blog.list_posts()
+    render(conn, :blog, posts: posts)
   end
 
   def projects(conn, _params) do
