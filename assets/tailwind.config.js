0bf6aaa58e6497ce0ac6558617f68d22401a95d0
Tristan Jahnke <trjahnke@protonmail.com>
2024-11-07 21:37:21 -0600

    feat: add workplace ticker

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
       colors: {
         "charcoal": {
           DEFAULT: "#394053",
